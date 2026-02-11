4ad17b23b11f73d32bf24452c4a724d15fcd8119
maneranveer111 <maneranveer111@gmail.com>
2026-02-11 16:35:48 +0530

    Time: 15 ms (32.38%) | Memory: 31.9 MB (83.46%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -5,8 +5,8 @@ public:
         sort(nums.begin(), nums.end());
         int sum = 0;
 
-        for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) sum += nums[i];
+        for(int i = 0; i < n; i += 2) {
+            sum += nums[i];
         }
 
         return sum;
