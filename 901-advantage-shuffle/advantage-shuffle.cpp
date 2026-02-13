8a9340f08e32bc0d2a0443abbb297e8225b90995
maneranveer111 <maneranveer111@gmail.com>
2026-02-13 11:45:03 +0530

    Time: 33 ms (77.89%) | Memory: 63.4 MB (95.89%) - LeetSync

---
+++ 901-advantage-shuffle/advantage-shuffle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        sort(nums1.begin(), nums1.end());
+        vector<int> idx(n);
+
+        for(int i = 0; i < n; i++) idx[i] = i;
+
+        sort(idx.begin(), idx.end(), [&](int a, int b){
+            return nums2[a] < nums2[b];
+        });
+
+        vector<int> ans(n);
+        int low = 0, high = n - 1;
+
+        for(int i = n - 1; i >= 0; i--) {
+            int j = idx[i];
+
+            if(nums1[high] > nums2[j]) {
+                ans[j] = nums1[high];
+                high--; 
+            }
+            else {
+                ans[j] = nums1[low];
+                low++;
+            }
+        }
+
+        return ans;
+     }
+};
