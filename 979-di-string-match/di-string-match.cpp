9a81693904b0b5fd313f8a67302e6fb92f6e2768
maneranveer111 <maneranveer111@gmail.com>
2026-02-21 22:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (32.48%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.size();
+        vector<int> ans;
+        
+        int low = 0, high = n;
+
+        for(char c : s) {
+            if(c == 'I') {
+                ans.push_back(low);
+                low++;
+            }
+            else { 
+                ans.push_back(high);
+                high--;
+            }
+        }
+
+        ans.push_back(low);  
+
+        return ans;
+    }
+};
