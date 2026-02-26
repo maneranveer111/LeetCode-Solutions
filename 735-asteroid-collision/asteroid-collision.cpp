f88863adc1e2b064612b996539c6001ddcf6d1c8
maneranveer111 <maneranveer111@gmail.com>
2026-02-26 22:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (40.42%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+
+        for(int a : asteroids) {
+            bool destroyed = false;
+
+            while(!st.empty() && a < 0 && st.top() > 0) {
+                if(st.top() < -a) {
+                    st.pop(); 
+                }
+                else if(st.top() == -a) {
+                    st.pop();  
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    destroyed = true;  
+                    break;
+                }
+            }
+
+            if(!destroyed) {
+                st.push(a);
+            }
+        }
+
+        vector<int> result(st.size());
+        for(int i = st.size() - 1; i >= 0; i--) {
+            result[i] = st.top();
+            st.pop();
+        }
+
+        return result;
+    }
+};
