86718692a849be91ac2f339e215fbbe4c5205f31
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-03-07 09:31:01 +0530

    Time: 3 ms (38.15%) | Memory: 17.2 MB (15.41%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int n = tokens.size();
-        stack<int> st;
+        stack<long long> st;
 
-        for(string s : tokens) {
-            int sz = s.size();
-            if(isdigit(s[sz - 1])) {
-                int num = 0;
-                if(isdigit(s[0])) {
-                    for(int i = 0; i < sz; i++) num = num * 10 + (s[i] - '0');
-                    st.push(num);
-                }
-                else {
-                    for(int i = 1; i < sz; i++) num = num * 10 + (s[i] - '0');
-                    st.push(0 - num);
-                }
+        for(string& s : tokens) {
+            if(s == "+" || s == "-" || s == "*" || s == "/") {
+                long long b = st.top(); st.pop();
+                long long a = st.top(); st.pop();
+
+                if(s == "+") st.push(a + b);
+                else if(s == "-") st.push(a - b);
+                else if(s == "*") st.push(a * b);
+                else if(s == "/") st.push(a / b);
             }
-            if(sz == 1 && s[0] == '+') {
-                int b = st.top();
-                st.pop();
-                int a = st.top();
-                st.pop();
-                int res = b + a;
-                st.push(res);
-            }
-            else if(sz == 1 && s[0] == '-') {
-                int b = st.top();
-                st.pop();
-                int a = st.top();
-                st.pop();
-                int res = a - b;
-                st.push(res);
-            }
-            else if(sz == 1 && s[0] == '*') {
-                int b = st.top();
-                st.pop();
-                int a = st.top();
-                st.pop();
-                int res = a * b;
-                st.push(res);
-            }
-            else if(sz == 1 && s[0] == '/') {
-                int b = st.top();
-                st.pop();
-                int a = st.top();
-                st.pop();
-                int res = a / b;
-                st.push(res);
+            else {
+                st.push(stoll(s));   
             }
         }
 
