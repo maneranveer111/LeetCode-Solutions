4ef058355b2de6349367b2ee5b73e8bd00119c55
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-04 08:12:17 +0530

    Time: 2 ms (77.10%) | Memory: 7.7 MB (88.23%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    // int helper(int n, vector<int>& dp){
+    //     if(n == 0 || n == 1) return n;
+    //     if(dp[n] != -1) return dp[n];
+
+    //     return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+    // }
+
+    int fib(int n) {
+        if(n <= 1)
+            return n;
+        int prev1 = 1, prev2 = 0;
+        int curr = 0;
+
+        for(int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
+    }
+};
