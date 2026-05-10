7767ad067ec8d4d1da9fdc9f58463a8d6b480cd5
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-10 17:28:32 +0530

    Time: 8 ms (96.58%) | Memory: 68.6 MB (40.06%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -30,8 +30,24 @@ public:
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
 
-        vector<int> dp(n, -2);
+        // vector<int> dp(n, -2);
+        // int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[0] = 0;
 
-        return solve(0, nums, target, dp);
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != -1) {
+                for (int j = i + 1; j < n; j++) {
+                    if (abs(nums[i] - nums[j]) <= target && dp[i] != -1) {
+                        dp[j] = max(dp[j], dp[i] + 1);
+                    }
+                }
+            } 
+        }
+
+        return dp[n - 1];
+
+
+        // return solve(0, nums, target, dp);
     }
 };
