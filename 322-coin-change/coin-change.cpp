66ec1990707d240208971524a96cc3bec2e7093c
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-12 18:22:26 +0530

    Time: 19 ms (78.67%) | Memory: 17.9 MB (80.34%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -23,11 +23,26 @@ public:
         return dp[idx][amount] = min(take, nontake);
     }
 
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n = coins.size();
+    //     vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
         
-        int ans = helper(coins, dp, amount, n - 1);
-        return (ans == INT_MAX) ? -1 : ans;
+    //     int ans = helper(coins, dp, amount, n - 1);
+    //     return (ans == INT_MAX) ? -1 : ans;
+    // }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++) {
+            for(int coin : coins) {
+                if(coin <= i && dp[i - coin] != INT_MAX)
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
     }
+
 };
