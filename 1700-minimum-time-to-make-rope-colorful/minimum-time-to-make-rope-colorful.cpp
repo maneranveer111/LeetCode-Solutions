4a4dd331e5d51c739a3f9285e022f19c3fa54867
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-15 21:32:02 +0530

    Time: 8 ms (24.77%) | Memory: 99.9 MB (33.59%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(int i, string& c, vector<int>& t, int n) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-        int j = i, sum = 0, mx = 0;
-        while (j < n && c[j] == c[i]) {
-            sum += t[j]; mx = max(mx, t[j]); j++;
-        }
-        return dp[i] = (sum - mx) + solve(j, c, t, n);
-    }
-    
     int minCost(string colors, vector<int>& neededTime) {
         int n = colors.size();
-        dp.assign(n, -1);
-        return solve(0, colors, neededTime, n);
+        int totalCost = 0;
+        int i = 0;
+        while (i < n) {
+            int j = i, sum = 0, mx = 0;
+            while (j < n && colors[j] == colors[i]) {
+                sum += neededTime[j];
+                mx = max(mx, neededTime[j]);
+                j++;
+            }
+            totalCost += (sum - mx);
+            i = j;
+        }
+        return totalCost;
     }
 };
