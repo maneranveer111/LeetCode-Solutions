def907d3caf0fd6191e4a3dc60106b43225b98e2
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-17 21:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (89.93%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // int n = nums.size();
-        // int lft = 0, rht = n - 1;
+        int n = nums.size();
+        int lft = 0, rht = n - 1;
 
-        // while(lft < rht) {
-        //     int mid = lft + (rht - lft) / 2;
+        while(lft < rht) {
+            int mid = lft + (rht - lft) / 2;
             
-        //     while(nums[lft] == nums[rht]) {
-        //         lft++;
-        //         rht--;
-        //         if(lft >= rht)
-        //             return nums[rht];
-        //     }
+            if(nums[mid] == nums[lft] && nums[mid] == nums[rht]) {
+                while(nums[lft] == nums[rht]) {
+                    lft++;
+                    rht--;
 
-        //     if(nums[mid] > nums[rht] && nums[mid] > nums[lft]) {
-        //         lft = mid + 1;
-        //     }
-        //     else if(nums[mid] < nums[rht] && nums[mid] <nums[lft]) {
-        //         rht = mid;
-        //     }
-        //     else if(nums[mid] > nums[rht] ) lft = mid + 1;
-        //     else rht = mid;
-        // }
+                    if(rht < lft)
+                        return nums[rht];
+                }
+            }
 
-        // return nums[lft];
-
-        int mini = INT_MAX;
-        for(int i = 0; i < nums.size(); i++) {
-            mini = min(mini, nums[i]); 
+            if(nums[mid] > nums[rht])
+                lft = mid + 1;
+            else 
+                rht = mid;
         }
 
-        return mini;
+        return nums[lft];
+
+        // int mini = INT_MAX;
+        // for(int i = 0; i < nums.size(); i++) {
+        //     mini = min(mini, nums[i]); 
+        // }
+
+        // return mini;
     }
 };
