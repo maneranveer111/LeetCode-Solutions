85856be256eadad259ba211ad3e04ede0984f05f
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-22 21:02:26 +0530

    Time: 15 ms (50.86%) | Memory: 28.4 MB (94.37%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<int> idx(intervals.size());
+        iota(idx.begin(), idx.end(), 0);
+
+        sort(idx.begin(), idx.end(), [&intervals](int i, int j){
+            return intervals[i][0] < intervals[j][0];
+        });   
+
+        // for(int i = 0; i < idx.size(); i++)
+        //     cout << idx[i] << " ";
+        // cout << endl; 
+
+        vector<int> ans(intervals.size());
+
+        for(int i = 0; i < intervals.size(); i++) {
+            int j = -1;
+
+            int lft = 0, rht = idx.size() - 1;
+
+            while(lft <= rht) {
+                int mid = lft + (rht - lft) / 2;
+
+                if(intervals[idx[mid]][0] >= intervals[i][1]) {
+                    j = idx[mid];
+                    rht = mid - 1;
+                }
+                else 
+                    lft = mid + 1;
+            }
+
+            ans[i] = j;
+        }
+
+        return ans;
+    }
+};
