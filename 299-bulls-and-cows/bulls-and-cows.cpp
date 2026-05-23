df26d13090b4822abf422fd26067e0f93145d51c
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-23 16:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (76.83%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        int bull = 0, cows = 0;
-        unordered_map<int, vector<int>> mp;
-        unordered_map<int, int> mp2;
-        unordered_map<int, int> mp3;
+        int bulls = 0, cows = 0;
+        int sec[10] = {0}, gue[10] = {0};
 
         for(int i = 0; i < secret.size(); i++) {
-            mp[secret[i]].push_back(i);
-            mp2[secret[i]]++;
-            mp3[guess[i]]++;
-        }    
-
-        for(int j = 0; j < guess.size(); j++) {
-            if(mp.find(guess[j]) != mp.end()) {
-                int temp = 0;
-                for(int i = 0; i < mp[guess[j]].size(); i++) {
-                    if(mp[guess[j]][i] == j){
-                        bull++;
-                        mp2[guess[j]]--;
-                        mp3[guess[j]]--;
-                    }
-                }
-            }
-        }
-        
-        for(int i = 0; i < secret.size(); i++) {
-            if(mp.find(guess[i]) != mp.end()) {
-                cows += min(mp2[guess[i]], mp3[guess[i]]);
-                mp.erase(guess[i]);
+            if(secret[i] == guess[i])
+                bulls++;
+            else {
+                sec[secret[i] - '0']++;
+                gue[guess[i] - '0']++;
             }
         }
 
-        return to_string(bull) + "A" + to_string(cows) + "B";
+        for(int i = 0; i < 10; i++)
+            cows += min(gue[i], sec[i]);
+
+        return to_string(bulls) + "A" + to_string(cows) + "B";
 
     }
 };
