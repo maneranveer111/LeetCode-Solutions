89ce8b38b837344e1c05568254362775ff0922b3
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-24 15:23:20 +0530

    Time: 2358 ms (5.15%) | Memory: 56.3 MB (5.15%) - LeetSync

---
+++ 639-decode-ways-ii/decode-ways-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int twoCharWays(char right) {
+        int d = right - '0';
+        if (d <= 6) return 2;   
+        else        return 1;   
+    }
+
+    const int MOD = 1e9 + 7;
+
+    int helper(string& s, int idx, vector<int>& memo) {
+        if(idx >= s.size())
+            return 1;
+         
+        if(memo[idx] != -1)
+            return memo[idx];
+
+        if(s[idx] == '*') {
+            int sStr = 0, strDig = 0, digStr = 0, strStr = 0;
+
+            sStr = 9LL * helper(s, idx + 1, memo) % MOD; 
+
+            if(idx != s.size() - 1) {
+                if(s[idx + 1] != '*') {
+                    int mul = twoCharWays(s[idx + 1]);
+
+                    strDig = mul * helper(s, idx + 2, memo) % MOD;
+                }
+                else {
+                    strStr = 15LL * helper(s, idx + 2, memo) % MOD;
+                }
+            }
+
+            return memo[idx] = (sStr +  strDig + strStr) % MOD;
+        }
+        else {
+            if(s[idx] == '0')
+            return memo[idx] = 0;
+
+            int single = 0;
+            if(s[idx] >= '1' && s[idx] <= '9')
+                single = helper(s, idx + 1, memo) % MOD;
+            
+            int two = 0;
+            if (idx != s.size() - 1 && s[idx] != '*') {
+                if (s[idx + 1] == '*') {
+                    if (s[idx] == '1')      
+                        two = 9LL * helper(s, idx + 2, memo) % MOD;
+                    else if (s[idx] == '2') 
+                        two = 6LL * helper(s, idx + 2, memo) % MOD;
+                } 
+                else if (stoi(s.substr(idx, 2)) <= 26) {
+                    two = helper(s, idx + 2, memo) % MOD;
+                }
+            }
+
+            return memo[idx] = (single + two) % MOD;
+        }
+
+        return memo[idx] = 0;
+    }
+
+    int numDecodings(string s) {
+        vector<int> memo(s.size(), -1);
+        return helper(s, 0, memo);
+    }
+};
+
