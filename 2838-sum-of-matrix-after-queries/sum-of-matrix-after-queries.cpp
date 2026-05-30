5747012fbc21e4846a8f2794211202311c5be0f0
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-05-30 08:40:46 +0530

    Time: 933 ms (5.38%) | Memory: 201.3 MB (37.63%) - LeetSync

--- 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     long long matrixSumQueries(int n, vector<vector<int>>& queries) {
         long long ans = 0;
-        set<int> row, col;
+        vector<int> row, col;
 
         for(int i = queries.size() - 1; i >= 0; i--) {
             int type = queries[i][0], val = queries[i][2], idx = queries[i][1];
             if(type == 0) {
-                if(row.find(idx) != row.end())
+                if(find(row.begin(), row.end(), idx) != row.end())
                     continue;
-                row.insert(idx);
+                row.push_back(idx);
                 int cnt = col.size();
                 ans += (val * n) - (cnt * val);
             }
             else if(type == 1) {
-                if(col.find(idx) != col.end())
+                if(find(col.begin(), col.end(), idx) != col.end())
                     continue;
-                col.insert(idx);
+                col.push_back(idx);
                 int cnt = row.size();
                 ans += (val * n) - (cnt * val);
             }
