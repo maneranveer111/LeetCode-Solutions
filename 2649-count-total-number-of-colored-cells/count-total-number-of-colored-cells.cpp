ca5096b3b80c887f4cca167a17758d65c4a8560e
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-06-01 11:10:33 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.75%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if(n == 1)
-            return 1;
-        long long diff = 4;
-        long long ans = 1;
+        // if(n == 1)
+        //     return 1;
+        // long long diff = 4;
+        // long long ans = 1;
 
-        for(long long i = 2; i <= n; i++) {
-            ans += diff;
-            diff += 4;
-        } 
 
-        return ans;
+        return (long long)2 * n * (n - 1) + 1;
+
+        // for(long long i = 2; i <= n; i++) {
+        //     ans += diff;
+        //     diff += 4;
+        // } 
+
+        // return ans;
     }
 };
