d0b32d8e782fe03ffb61ddadeb7a5353bc1dae28
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-06-03 18:26:57 +0530

    Time: 4 ms (73.39%) | Memory: 237.9 MB (92.66%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -2,72 +2,30 @@ class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int n = landStartTime.size(), m = waterStartTime.size();
-        vector<pair<int, int>> land(n), water(m);
-        
-        for(int i = 0; i < n; i++)
-            land[i] = {landStartTime[i], landDuration[i]};
-
-        for(int j = 0; j < m; j++)
-            water[j] = {waterStartTime[j], waterDuration[j]};
-        
-        sort(land.begin(), land.end());
-        sort(water.begin(), water.end());
-
-        vector<int> prefixL;
-        vector<int> prefixW;
-        int till = INT_MAX;
+        int minLand = INT_MAX, minWater = INT_MAX;
 
         for(int i = 0; i < n; i++) {
-            if(!prefixL.empty()) {
-                till = min(till, land[i].first + land[i].second);
-                if(prefixL.back() != till)
-                    prefixL.push_back(till);
-            } else {
-                prefixL.push_back(land[i].first + land[i].second);
-                till = land[i].first + land[i].second;
-            }
+            minLand = min(minLand, landStartTime[i] + landDuration[i]);
         }
 
-        till = INT_MAX;
-        for(int j = 0; j < m; j++) {
-            if(!prefixW.empty()) {
-                till = min(till, water[j].first + water[j].second);
-                if(prefixW.back() != till)
-                    prefixW.push_back(till);
-            } else {
-                prefixW.push_back(water[j].first + water[j].second);
-                till = water[j].first + water[j].second;
-            }
+        for(int i = 0; i < m; i++) {
+            minWater = min(minWater, waterStartTime[i] + waterDuration[i]);
         }
-
         int ans = INT_MAX;
 
-        for(int i = 0; i < (int)prefixL.size(); i++) {
-            int best = INT_MAX;
-            int time = prefixL[i];
-            for(int j = 0; j < m; j++) {
-                int cur = time;
-                if(cur <= water[j].first)
-                    cur = water[j].first + water[j].second;
-                else
-                    cur += water[j].second;
-                best = min(best, cur);
+        for(int j = 0; j < m; j++) {
+            if(minLand >= waterStartTime[j]) {
+                ans = min(ans, minLand + waterDuration[j]);
             }
-            ans = min(ans, best);
+            else 
+                ans = min(ans, waterDuration[j] + waterStartTime[j]);
         }
 
-        for(int i = 0; i < (int)prefixW.size(); i++) {
-            int best = INT_MAX;
-            int time = prefixW[i];
-            for(int j = 0; j < n; j++) {
-                int cur = time;
-                if(cur <= land[j].first)
-                    cur = land[j].first + land[j].second;
-                else
-                    cur += land[j].second;
-                best = min(best, cur);
-            }
-            ans = min(ans, best);
+        for(int i = 0; i < n; i++) {
+            if(minWater >= landStartTime[i])
+                ans = min(ans, minWater + landDuration[i]);
+            else 
+                ans = min(ans, landDuration[i] + landStartTime[i]);
         }
 
         return ans;
