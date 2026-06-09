b562b4af78c449b4e2bb6686a05e2f86bdb2e422
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-06-09 18:58:06 +0530

    Time: 2 ms (58.28%) | Memory: 9.4 MB (25.32%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+
     bool helper(string& s, string& p, int i, int j) {
         if(i == s.size() && j == p.size())
             return true;
@@ -11,24 +13,28 @@ public:
         
         if(j >= p.size())
             return false;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
 
         if(j + 1 < p.size() && p[j + 1] == '*') {
             if(s[i] == p[j] || p[j] == '.') {
                 if(helper(s, p, i + 1, j))
-                    return true;
+                    return dp[i][j] = true;
             }
-            return helper(s, p, i, j + 2);
+            return dp[i][j] = helper(s, p, i, j + 2);
         }
         else {
             if(s[i] == p[j] || p[j] == '.') {
-                return helper(s, p, i + 1, j + 1);
+                return dp[i][j] = helper(s, p, i + 1, j + 1);
             }
         }
 
-        return false;
+        return dp[i][j] = false;
     }
 
     bool isMatch(string s, string p) {
+        dp.assign(s.size(), vector<int>(p.size(), -1));
         return helper(s, p, 0, 0);
     }
 };
