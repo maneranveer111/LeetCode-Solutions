a6fb29745e4747462f84f60d06ebf23509e3ece5
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-06-16 10:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (87.50%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] >= 'a' && s[i] <= 'z')
+                result += s[i];
+            else if(s[i] == '*' && !result.empty())
+                result.pop_back();
+            else if(s[i] == '#' && !result.empty())
+                result += result;
+            else if(s[i] == '%' && !result.empty())
+                reverse(result.begin(), result.end());
+        }
+
+        return result;
+    }
+};
