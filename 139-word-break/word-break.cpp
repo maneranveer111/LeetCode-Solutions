3494bbdce0a443a9af567184054ac99f7b3e14ba
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-06-18 16:51:11 +0530

    Time: 3 ms (73.27%) | Memory: 12.8 MB (64.99%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,5 +1,77 @@
-struct Trie{
-    Trie *child[26];
+// struct Trie{
+//     Trie *child[26];
+//     bool isEnd;
+
+//     Trie() : isEnd(false) {
+//         fill(child, child + 26, nullptr);
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> memo;
+
+//     Trie* root;
+//     void insert(Trie* root, string& word) {
+//         Trie* curr = root;
+
+//         for(char ch : word) {
+//             if(!curr->child[ch - 'a'])
+//                 curr->child[ch - 'a'] = new Trie();
+            
+//             curr = curr->child[ch - 'a'];
+//         }
+
+//         curr->isEnd = true;
+//     }        
+
+//     bool isPresent(Trie* root, string word) {
+//         Trie* curr = root;
+
+//         for(char ch : word) {
+//             if(!curr->child[ch - 'a'])
+//                 return false;
+            
+//             curr = curr->child[ch - 'a'];
+//         }
+
+//         if(curr->isEnd)
+//             return true;
+
+//         return false;
+//     }
+
+//     bool helper(string& s, int idx) {
+//         if(idx == s.size())
+//             return true;
+//         if(memo[idx] != -1)
+//             return memo[idx];
+
+//         for(int i = idx; i < s.size(); i++) {
+//             if(isPresent(root, s.substr(idx, i - idx + 1))) {
+//                 bool callnext = helper(s, i + 1);
+//                 if(callnext)
+//                     return true;
+//             }
+//         }
+
+//         return memo[idx] = false;
+//     }
+
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         root = new Trie();
+//         memo.assign(s.size() + 1, -1);
+        
+//         for(string word : wordDict) {
+//             insert(root, word);
+//         }
+        
+//         return helper(s, 0);
+//     }
+// };
+
+struct Trie {
+    Trie* child[26];
     bool isEnd;
 
     Trie() : isEnd(false) {
@@ -9,63 +81,39 @@ struct Trie{
 
 class Solution {
 public:
-    vector<int> memo;
-
     Trie* root;
+
     void insert(Trie* root, string& word) {
         Trie* curr = root;
-
-        for(char ch : word) {
-            if(!curr->child[ch - 'a'])
-                curr->child[ch - 'a'] = new Trie();
-            
-            curr = curr->child[ch - 'a'];
+        for (int i = word.size() - 1; i >= 0; i--) {
+            int ch = word[i] - 'a';
+            if (!curr->child[ch])
+                curr->child[ch] = new Trie();
+            curr = curr->child[ch];
         }
-
         curr->isEnd = true;
-    }        
-
-    bool isPresent(Trie* root, string word) {
-        Trie* curr = root;
-
-        for(char ch : word) {
-            if(!curr->child[ch - 'a'])
-                return false;
-            
-            curr = curr->child[ch - 'a'];
-        }
-
-        if(curr->isEnd)
-            return true;
-
-        return false;
-    }
-
-    bool helper(string& s, int idx) {
-        if(idx == s.size())
-            return true;
-        if(memo[idx] != -1)
-            return memo[idx];
-
-        for(int i = idx; i < s.size(); i++) {
-            if(isPresent(root, s.substr(idx, i - idx + 1))) {
-                bool callnext = helper(s, i + 1);
-                if(callnext)
-                    return true;
-            }
-        }
-
-        return memo[idx] = false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         root = new Trie();
-        memo.assign(s.size() + 1, -1);
-        
-        for(string word : wordDict) {
+        for (string& word : wordDict)
             insert(root, word);
+
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            Trie* curr = root;
+            for (int j = i - 1; j >= 0; j--) {
+                int ch = s[j] - 'a';
+                if (!curr->child[ch]) break;
+                curr = curr->child[ch];
+                if (curr->isEnd && dp[j])
+                    dp[i] = true;
+            }
         }
-        
-        return helper(s, 0);
+
+        return dp[n];
     }
 };
