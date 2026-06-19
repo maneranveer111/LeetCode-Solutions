b9418929152565d8b2d5906d3880b99aa0970f8d
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-06-19 21:53:20 +0530

    Time: 519 ms (5.02%) | Memory: 121.1 MB (7.35%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    long long helper2(long long n1, long long n2, char op, long long last) {
+        if(op == '+')
+            return n1 + n2;
+        else if(op == '-')
+            return n1 - n2;
+        else if(op == '*') {
+            return (n1 - last) + (last * n2);
+        }
+        return 0;
+    }
+
+    void helper(string& num, string& curr, long long val, int idx, long long target, char op, long long last) {
+        if(idx == num.size()) {
+            if(val == target)
+                ans.push_back(curr);
+            return;
+        }
+        long long val1 = val;
+
+        for(int i = idx; i < num.size(); i++) {
+            string num1 = num.substr(idx, i - idx + 1);
+            if(num1.size() > 1 && num1[0] == '0')
+                break;
+
+            long long nm = stoll(num1);
+            val = helper2(val1, nm, op, last);
+
+            long long nextLast;
+            if(op == '+') nextLast = nm;
+            else if(op == '-') nextLast = -nm;
+            else nextLast = last * nm;
+
+            curr += num1;
+            if(i + 1 < num.size()) {
+                curr.push_back('+');
+                helper(num, curr, val, i + 1, target, '+', nextLast);
+                curr.pop_back();
+                curr.push_back('-');
+                helper(num, curr, val, i + 1, target, '-', nextLast);
+                curr.pop_back();
+                curr.push_back('*');
+                helper(num, curr, val, i + 1, target, '*', nextLast);
+                curr.pop_back();
+            } else {
+                helper(num, curr, val, i + 1, target, op, nextLast);
+            }
+
+            curr = curr.substr(0, curr.size() - num1.size());
+        }
+    }
+
+    vector<string> addOperators(string num, int target) {
+        string curr = "";
+        helper(num, curr, 0, 0, (long long)target, '+', 0);
+        return ans;
+    }
+};
