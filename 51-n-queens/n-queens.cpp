a3cb033aa0324f47318f28436962f7b71ba8b4bb
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-07-04 10:13:05 +0530

    Time: 1 ms (78.45%) | Memory: 10.4 MB (49.10%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-
-    void solver(int col, int n, vector<string>& boards, vector<int>& leftCol, 
-        vector<int>&lowerDig, vector<int>& upperDig, vector<vector<string>>& ans) {
-            if(col == n) {
-                ans.push_back(boards);
-                return;
-            }
-
-            for(int row = 0; row < n; row++) {
-                if(leftCol[row] == 0 && lowerDig[col + row] == 0 &&
-                    upperDig[n - 1 + col - row] == 0) {
-                        boards[row][col] = 'Q';
-                        leftCol[row] = 1;
-                        lowerDig[col + row] = 1;
-                        upperDig[n - 1 + col - row] = 1;
-                        solver(col + 1, n, boards, leftCol, lowerDig, upperDig, ans);
-                        boards[row][col] = '.';
-                        leftCol[row] = 0;
-                        lowerDig[col + row] = 0;
-                        upperDig[n - 1 + col - row] = 0;
-                    }
-            }
+    void solver(int col, int n, vector<int>& lowerdig, vector<int>& upperdig, vector<int>& leftcol,
+            vector<string>& board, vector<vector<string>>& ans) {
+        if(col == n) {
+            ans.push_back(board);
+            return;
         }
 
+        for(int row = 0; row < n; row++) {
+            if(leftcol[row] == 0 && lowerdig[col + row] == 0 && upperdig[n - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftcol[row] = 1;
+                lowerdig[col + row] = 1;
+                upperdig[n - 1 + col - row] = 1;
+                solver(col + 1, n, lowerdig, upperdig, leftcol, board, ans);
+                board[row][col] = '.';
+                leftcol[row] = 0;
+                lowerdig[col + row] = 0;
+                upperdig[n - 1 + col - row] = 0;
+            }
+        }
+    }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> boards(n);
+        vector<string> board(n);
         vector<vector<string>> ans;
         string s(n, '.');
 
         for(int i = 0; i < n; i++) 
-            boards[i] = s;
+            board[i] = s;
         
-        vector<int> leftCol(n, 0), lowerDig(2 * n - 1, 0), upperDig(2 * n - 1, 0);
+        vector<int> leftcol(n, 0), lowerdig(2 * n - 1, 0), upperdig(2 * n - 1, 0);
 
-        solver(0, n, boards, leftCol, lowerDig, upperDig, ans);
+        solver(0, n, lowerdig, upperdig, leftcol, board, ans);
 
         return ans;
+        
     }
 };
