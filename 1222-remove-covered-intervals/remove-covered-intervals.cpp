c410e6581cf12a1ff6d8974e239719cc4a0f276b
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-07-06 09:03:40 +0530

    Time: 70 ms (8.08%) | Memory: 15 MB (82.12%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            if (a[0] == b[0]) {
+                return a[1] > b[1]; 
+            }
+            return a[0] < b[0];    
+        });
+
+        for(int i = 0; i < intervals.size(); i++)
+            cout << intervals[i][0] << ", " << intervals[i][1] << endl;
+        
+        pair<int, int> prev = {intervals[0][0], intervals[0][1]};
+        int cnt = 1;
+
+        for(int i = 1; i < intervals.size(); i++) {
+            if(prev.first <= intervals[i][0] && intervals[i][1] <= prev.second)
+                continue;
+            else {
+                cnt++;
+                prev.first = min(intervals[i][0], prev.first);  
+                prev.second = max(intervals[i][1], prev.second);
+            }
+        }
+        
+        return cnt;
+    }
+};
