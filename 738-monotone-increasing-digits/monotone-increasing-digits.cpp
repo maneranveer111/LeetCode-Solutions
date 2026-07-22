b2003410072608221f608ac4d7eb409e16ee3553
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-07-22 22:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (6.07%) - LeetSync

---
+++ 738-monotone-increasing-digits/monotone-increasing-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool helper(int idx, string& curr, string& num) {
+        if (idx == num.size())
+            return curr <= num;   // validate the full number, not just prefixes
+
+        if (curr > num.substr(0, idx))
+            return false;
+
+        char start = curr.empty() ? '0' : curr.back();
+
+        for (char ch = '9'; ch >= start; ch--) {
+            curr.push_back(ch);
+            if (helper(idx + 1, curr, num))
+                return true;
+            curr.pop_back();
+        }
+
+        return false;
+    }
+
+    int monotoneIncreasingDigits(int n) {
+        if (n < 10)
+            return n;
+
+        string num = to_string(n);
+        string curr = "";
+        helper(0, curr, num);
+        return stoi(curr);
+    }
+};
