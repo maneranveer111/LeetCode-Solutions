c206f6d4462f6dbf9475f8d90ef9d5223ce1920b
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-11 15:02:06 +0530

    Time: 2 ms (16.54%) | Memory: 14.6 MB (53.01%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> vis;
+
+    void helper(int node, vector<vector<int>>& rooms) {
+        vis[node] = 1;
+
+        for(auto nei : rooms[node]) {
+            if(vis[nei])
+                continue;
+            helper(nei, rooms);
+        }
+    }
+
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vis.assign(n, 0);
+        helper(0, rooms);
+        for(int i = 0; i < n; i++)
+            if(!vis[i])
+                return false;
+
+        return true;
+    }
+};
