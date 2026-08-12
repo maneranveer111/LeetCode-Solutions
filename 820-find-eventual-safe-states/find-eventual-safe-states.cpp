a97b67b082662c3d356c2de8c755ed9e3fca9b22
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-12 15:19:39 +0530

    Time: 53 ms (32.05%) | Memory: 66.1 MB (47.99%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -24,20 +24,55 @@ public:
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int v = graph.size();
-        vector<int> vis(v, -1), pathVis(v, -1), check(v, 0);
-        vector<int> ans;
+        // vector<int> vis(v, -1), pathVis(v, -1), check(v, 0);
+        // vector<int> ans;
         
-        for(int i = 0; i < v; i++) {
-            if(vis[i] != -1)
-                continue;
+        // for(int i = 0; i < v; i++) {
+        //     if(vis[i] != -1)
+        //         continue;
 
-            dfs(i, vis, pathVis, check, graph);
+        //     dfs(i, vis, pathVis, check, graph);
+        // }
+
+        // for(int i = 0; i < v; i++)
+        //     if(check[i] == 1)
+        //         ans.push_back(i);
+            
+        // return ans;
+
+        vector<int> outDeg(v, 0);
+        vector<vector<int>> reverseGraph(v);
+
+        for(int i = 0; i < v; i++) {
+            int sz = graph[i].size();
+            outDeg[i] = sz;
+
+            for(auto nei : graph[i]) {
+                reverseGraph[nei].push_back(i);
+            }
         }
 
-        for(int i = 0; i < v; i++)
-            if(check[i] == 1)
-                ans.push_back(i);
-            
-        return ans;
+        queue<int> q;
+        for(int i = 0; i < v; i++) 
+            if(outDeg[i] == 0)
+                q.push(i);
+        
+        vector<int> result;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            result.push_back(node);
+
+            for(auto nei : reverseGraph[node]) {
+                outDeg[nei]--;
+                if(outDeg[nei] == 0)
+                    q.push(nei);
+            }
+        }
+
+        sort(result.begin(), result.end());
+
+        return result;
     }
 };
