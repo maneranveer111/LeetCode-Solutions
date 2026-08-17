28f0b49f9d8069150c3e8ef07c95822c558e4fc4
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-17 18:20:10 +0530

    Time: 655 ms (33.81%) | Memory: 27.9 MB (21.68%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
+    // vector<vector<int>> memo;
     vector<int> preSum;
 
-    int helper(int i, int j) {
-        if(i >= j)
-            return 0;
-        if(memo[i][j] != -1)
-            return memo[i][j];
-        int ans = 0;
+    // int helper(int i, int j) {
+    //     if(i >= j)
+    //         return 0;
+    //     if(memo[i][j] != -1)
+    //         return memo[i][j];
+    //     int ans = 0;
 
-        for(int k = i; k < j; k++) {
-            int left = preSum[k] - preSum[i - 1];
-            int rht = preSum[j] - preSum[k];
+    //     for(int k = i; k < j; k++) {
+    //         int left = preSum[k] - preSum[i - 1];
+    //         int rht = preSum[j] - preSum[k];
 
-            if(left > rht)
-                ans = max(ans, rht + helper(k + 1, j));
-            else if(rht > left)
-                ans = max(ans, left + helper(i, k));
-            else {
-                ans = max(ans, left + max(helper(i, k), helper(k + 1, j)));
-            }    
-        }
+    //         if(left > rht)
+    //             ans = max(ans, rht + helper(k + 1, j));
+    //         else if(rht > left)
+    //             ans = max(ans, left + helper(i, k));
+    //         else {
+    //             ans = max(ans, left + max(helper(i, k), helper(k + 1, j)));
+    //         }    
+    //     }
 
-        return memo[i][j] = ans;
-    }
+    //     return memo[i][j] = ans;
+    // }
 
     int stoneGameV(vector<int>& stoneValue) {
         int n = stoneValue.size();
         if(n == 1)
             return 0;
         
-        memo.assign(n + 1, vector<int>(n + 1, -1));
+        // memo.assign(n + 1, vector<int>(n + 1, -1));
         preSum.assign(n + 1, 0);
         int sum = 0;
 
