b63c0ff5dbbcf1d7933b8e00e58e495498ff6dd5
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-18 15:48:55 +0530

    Time: 74 ms (60.37%) | Memory: 12.4 MB (26.26%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     vector<int> memo;
+    vector<vector<bool>> isPalin;
 
-    bool isPalindrome(string& s, int srt, int en) {
-        while(srt <= en) {
-            if(s[srt] != s[en])
-                return false;
-            srt++;
-            en--;
+    void helperPalindrome(string& s) {
+        int n = s.size();
+        isPalin.assign(n, vector<bool>(n, false));
+
+        for (int i = 0; i < n; i++)
+            isPalin[i][i] = true;
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j])
+                    isPalin[i][j] = (len == 2) || isPalin[i + 1][j - 1];
+            }
         }
-
-        return true;
     }
 
     int helper(int idx, string& s) {
-        if(idx == s.size())
+        if (idx == s.size())
             return 0;
-        if(memo[idx] != -1)
+        if (memo[idx] != -1)
             return memo[idx];
 
         int ans = INT_MAX;
-        
-        for(int i = idx; i < s.size(); i++) {
-            if(isPalindrome(s, idx, i)) {
+
+        for (int i = idx; i < s.size(); i++) {
+            if (isPalin[idx][i]) {                 
                 int curr = 1 + helper(i + 1, s);
                 ans = min(ans, curr);
             }
@@ -34,7 +40,8 @@ public:
     int minCut(string s) {
         int n = s.size();
         memo.assign(n, -1);
+        helperPalindrome(s);                
 
-        return helper(0, s) - 1;   
+        return helper(0, s) - 1;
     }
 };
