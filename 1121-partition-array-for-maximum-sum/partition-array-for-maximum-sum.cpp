58f8ebaad4c8d0afb4dbc3d92964b24017962479
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-18 16:08:04 +0530

    Time: 1 ms (91.40%) | Memory: 12.5 MB (31.93%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    vector<int> memo;
+    // vector<int> memo;
 
-    int helper(int idx, int k, vector<int>& arr) {
-        if(idx == arr.size())
-            return 0;
-        if(memo[idx] != -1)
-            return memo[idx];
+    // int helper(int idx, int k, vector<int>& arr) {
+    //     if(idx == arr.size())
+    //         return 0;
+    //     if(memo[idx] != -1)
+    //         return memo[idx];
 
-        int currMax = -1;   
-        int ans = INT_MIN;
+    //     int currMax = -1;   
+    //     int ans = INT_MIN;
 
-        for(int i = idx; i - idx + 1 <= k && i < arr.size(); i++) {
-            currMax = max(currMax, arr[i]);
-            int curr = (i - idx + 1) * currMax + helper(i + 1, k, arr);
-            ans = max(ans, curr);
-        }
+    //     for(int i = idx; i - idx + 1 <= k && i < arr.size(); i++) {
+    //         currMax = max(currMax, arr[i]);
+    //         int curr = (i - idx + 1) * currMax + helper(i + 1, k, arr);
+    //         ans = max(ans, curr);
+    //     }
 
-        return memo[idx] = ans;
-    }
+    //     return memo[idx] = ans;
+    // }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        memo.assign(n, -1);
+        // memo.assign(n, -1);
 
-        return helper(0, k, arr);  
+        vector<int> dp(n + 1, 0);
+
+        for(int idx = n - 1; idx >= 0; idx--) {
+            int currMax = -1;
+            int ans = -1;
+
+            for(int i = idx; i - idx + 1 <= k && i < n; i++) {
+                currMax = max(currMax, arr[i]);
+                int curr = currMax * (i - idx + 1) + dp[i + 1];
+                ans = max(ans, curr);
+            }
+
+            dp[idx] = ans;
+        }
+
+        // return helper(0, k, arr);  
+        return dp[0];
     }
 };
