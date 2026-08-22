cbd87f4888f49ddd1ece2d406699ddbd30be6f7a
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-22 10:33:13 +0530

    Time: 35 ms (57.21%) | Memory: 46.5 MB (15.44%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<string> watchedVideosByFriends(vector<vector<string>>& watchedVideos, vector<vector<int>>& friends, int id, int level) {
+        int n = watchedVideos.size();
+        int currLev = 0;
+        queue<int> q;
+        q.push(id);
+        vector<int> fri;
+        vector<int> vis(n, 0);
+        vis[id] = 1;
+
+        while(!q.empty()) {
+            int sz = q.size();
+            if(currLev == level) {
+                while(!q.empty()) {
+                    fri.push_back(q.front());
+                    q.pop();
+                }
+                break;
+            }
+
+            for(int i = 0; i < sz; i++) {
+                int node = q.front();
+                q.pop();
+
+                for(auto nei : friends[node]) {
+                    if(!vis[nei]) {
+                        vis[nei] = 1;
+                        q.push(nei);
+                    }
+                }
+            }
+
+            currLev++;
+        }
+
+
+        unordered_map<string, int> freq;
+        for(int fid : fri) {
+            for(string s : watchedVideos[fid]) {
+                freq[s]++;
+            }
+        }
+
+        vector<pair<int, string>> vec;
+        for(auto [s, feq] : freq) {
+            vec.push_back({feq, s});
+        }
+
+        sort(vec.begin(), vec.end(), [](auto a, auto b) {
+            if(a.first == b.first)
+                return a.second < b.second;
+            return a.first < b.first;
+        });
+
+        vector<string> ans;
+
+        for(auto v : vec) {
+            ans.push_back(v.second);
+        }
+
+        return ans;
+    }
+};
