04d22d88685dd1601eec5c7e4190a2ddcba03715
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-28 08:40:29 +0530

    Time: 75 ms (50.97%) | Memory: 48.4 MB (63.75%) - LeetSync

---
+++ 676-implement-magic-dictionary/implement-magic-dictionary.cpp
@@ -0,0 +1,42 @@
+class MagicDictionary {
+public:
+    vector<string> vec;
+    MagicDictionary() {
+        
+    }
+    
+    void buildDict(vector<string> dictionary) {
+        vec = dictionary;
+    }
+    
+    bool search(string searchWord) {
+        bool match = false;
+
+        for(string s : vec) {
+            if(s.size() != searchWord.size())
+                continue;
+            else {
+                int mismatch = 0;
+                for(int i = 0; i < s.size(); i++) {
+                    if(s[i] != searchWord[i]) {
+                        mismatch++;
+                    }
+                } 
+                
+                if(mismatch == 1) {
+                    cout << s << endl;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
