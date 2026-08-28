f42428a9f0267ab6201a925ffaec65eabfde4ee8
Mane Ranveer Pramod <maneranveer111@gmail.com>
2026-08-28 08:44:40 +0530

    Time: 44 ms (76.86%) | Memory: 102.3 MB (46.76%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.cpp
+++ 676-implement-magic-dictionary/implement-magic-dictionary.cpp
@@ -1,41 +1,54 @@
 class MagicDictionary {
 public:
-    vector<string> vec;
-    MagicDictionary() {
-        
-    }
-    
-    void buildDict(vector<string> dictionary) {
-        vec = dictionary;
-    }
-    
-    bool search(string searchWord) {
-        bool match = false;
+    struct TrieNode {
+        TrieNode* children[26] = {nullptr};
+        bool isEnd = false;
+    };
 
-        for(string s : vec) {
-            if(s.size() != searchWord.size())
-                continue;
-            else {
-                int mismatch = 0;
-                for(int i = 0; i < s.size(); i++) {
-                    if(s[i] != searchWord[i]) {
-                        mismatch++;
-                    }
-                } 
-                
-                if(mismatch == 1) {
+    TrieNode* root;
+
+    MagicDictionary() {
+        root = new TrieNode();
+    }
+
+    void buildDict(vector<string> dictionary) {
+        for (const string& word : dictionary) {
+            TrieNode* node = root;
+            for (char c : word) {
+                int idx = c - 'a';
+                if (!node->children[idx])
+                    node->children[idx] = new TrieNode();
+                node = node->children[idx];
+            }
+            node->isEnd = true;
+        }
+    }
+
+    bool search(string searchWord) {
+        return dfs(root, searchWord, 0, false);
+    }
+
+private:
+    bool dfs(TrieNode* node, const string& word, int i, bool used) {
+        if (!node) return false;
+
+        if (i == word.size())
+            return used && node->isEnd;
+
+        int idx = word[i] - 'a';
+
+        for (int c = 0; c < 26; c++) {
+            if (!node->children[c]) continue;
+
+            if (c == idx) {
+                if (dfs(node->children[c], word, i + 1, used))
                     return true;
-                }
+            } else if (!used) {
+                if (dfs(node->children[c], word, i + 1, true))
+                    return true;
             }
         }
 
         return false;
     }
 };
-
-/**
- * Your MagicDictionary object will be instantiated and called as such:
- * MagicDictionary* obj = new MagicDictionary();
- * obj->buildDict(dictionary);
- * bool param_2 = obj->search(searchWord);
- */
